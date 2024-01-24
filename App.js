64aca7f7cfa34e1006dd2a64366c3b77057eff81
FedeCasper <federicorouyere@gmail.com>
2024-01-24 16:57:55 -0300

    Create initial project structure

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hello Native World!</Text>
+      <Text style={styles.subtitle}>Federico Rouyere</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+export default App
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8c8c8c',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    color: '#4c007d',
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    fontSize: 30
+  },
+  subtitle:{
+    color: '#4c007d',
+    fontSize: 20
+  }
 });
